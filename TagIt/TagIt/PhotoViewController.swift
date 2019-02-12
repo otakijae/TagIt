76824f0eac98742f60732831e0e732516d4ddc50
ninetyfivejae <jh33836782@gmail.com>
2019-02-12 23:59:14 +0900

    Add: 앱 아이콘 추가 및 로고 추가
    - 앱 아이콘 추가
    - 앱 로고 추가 후 LaunchScreen에서 로고 이미지 보여줌
    - Navigation Title 로고 이미지로 설정

--- TagIt/TagIt/PhotoViewController.swift
+++ TagIt/TagIt/PhotoViewController.swift
@@ -17,7 +17,27 @@ class PhotoViewController: UICollectionViewController, UICollectionViewDelegateF
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        addNavBarImage()
+        
         grabPhotos()
+    }
+    
+    func addNavBarImage() {
+        let navController = navigationController!
+        
+        let image = UIImage(named: "logoColored.png")
+        let imageView = UIImageView(image: image)
+        
+        let bannerWidth = navController.navigationBar.frame.size.width
+        let bannerHeight = navController.navigationBar.frame.size.height
+        
+        let bannerX = bannerWidth / 2 - (image?.size.width)! / 2
+        let bannerY = bannerHeight / 2 - (image?.size.height)! / 2
+        
+        imageView.frame = CGRect(x: bannerX, y: bannerY, width: bannerWidth, height: bannerHeight)
+        imageView.contentMode = .scaleAspectFit
+        
+        navigationItem.titleView = imageView
     }
     
     func grabPhotos() {
