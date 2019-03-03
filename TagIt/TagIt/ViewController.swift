45584004e1696dc1c03c3334af3bca942d11eb49
nami77.kim <nami77.kim@linecorp.com>
2019-03-03 14:03:09 +0900

    Add comments in ViewController

--- TagIt/TagIt/ViewController.swift
+++ TagIt/TagIt/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    //남이가 이렇게 고쳤다~~이거 포크~
 
 
 }
